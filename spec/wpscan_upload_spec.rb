d95b4f745f5fcdf2455cace2ee27c260b2fb2098
Xavi Vila <xavivm@gmail.com>
2019-12-27 12:51:34 +0100

    test the real import

--- spec/wpscan_upload_spec.rb
+++ spec/wpscan_upload_spec.rb
@@ -28,40 +28,31 @@ describe 'wpscan upload plugin' do
       @importer.import(file: 'spec/fixtures/files/invalid.json')
     end
 
-    # it "creates nodes, issues, notes and an evidences as needed" do
-    #   expect(@content_service).to receive(:create_node) do |args|
-    #     # puts "create_node: #{ args.inspect }"
-    #     expect(args[:label]).to eq('74.207.244.221')
-    #     expect(args[:type]).to eq(:host)
-    #     OpenStruct.new(args)
-    #   end.once
-    #   expect(@content_service).to receive(:create_note) do |args|
-    #     puts "create_note: #{ args.inspect }"
-    #     expect(args[:text]).to include("#[Title]#\nWpscan info: 74.207.244.221")
-    #     expect(args[:text]).to_not include("not recognized by the plugin")
-    #     expect(args[:node].label).to eq("74.207.244.221")
-    #     OpenStruct.new(args)
-    #   end.once
-    #   expect(@content_service).to receive(:create_note) do |args|
-    #     puts "create_note: #{ args.inspect }"
-    #     expect(args[:text]).to include("#[Title]#\n22/tcp is open (syn-ack)")
-    #     expect(args[:text]).to_not include("not recognized by the plugin")
-    #     expect(args[:text]).to include("#[Host]#\n74.207.244.221")
-    #     expect(args[:node].label).to eq("74.207.244.221")
-    #     OpenStruct.new(args)
-    #   end.once
-    #   expect(@content_service).to receive(:create_note) do |args|
-    #     puts "create_note: #{ args.inspect }"
-    #     expect(args[:text]).to include("#[Title]#\n80/tcp is open (syn-ack)")
-    #     expect(args[:text]).to_not include("not recognized by the plugin")
-    #     expect(args[:text]).to include("#[Host]#\n74.207.244.221")
-    #     expect(args[:node].label).to eq("74.207.244.221")
-    #     OpenStruct.new(args)
-    #   end.once
-    #
-    #   # Run the import
-    #   @importer.import(file: 'spec/fixtures/files/sample.json')
-    # end
+    it "creates nodes, issues, notes and an evidences as needed" do
+      expect(@content_service).to receive(:create_node) do |args|
+        # puts "create_node: #{ args.inspect }"
+        expect(args[:label]).to eq('http://www.redacted.com/')
+        expect(args[:type]).to eq(:host)
+        OpenStruct.new(args)
+      end.once
+      expect(@content_service).to receive(:create_note) do |args|
+        # puts "create_note: #{ args.inspect }"
+        expect(args[:text]).to include("#[Title]#\nWPScan Scan Information")
+        expect(args[:node].label).to eq('http://www.redacted.com/')
+        OpenStruct.new(args)
+      end.once
+      expect(@content_service).to receive(:create_issue) do |args|
+        # puts "create_issue: #{ args.inspect }"
+        OpenStruct.new(args)
+      end.exactly(10).times
+      expect(@content_service).to receive(:create_evidence) do |args|
+        # puts "create_evidence: #{ args.inspect }"
+        OpenStruct.new(args)
+      end.exactly(5).times
+
+      # Run the import
+      @importer.import(file: 'spec/fixtures/files/sample.json')
+    end
 
   end
 end
