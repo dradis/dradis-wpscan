25f156bc160e8dd8c82ef65f92d1b450b7a541ef
Matt Budz <matt.budz@protonmail.com>
2024-03-08 11:59:40 +0100

    add default mappings

--- lib/dradis/plugins/wpscan/mapping.rb
+++ lib/dradis/plugins/wpscan/mapping.rb
@@ -1,6 +1,29 @@
 module Dradis::Plugins::Wpscan
   module Mapping
     def self.default_mapping
+      {
+        'evidence' => {
+          'Evidence' => '{{ wpscan[evidence.evidence] }}'
+        },
+        'scan_info' => {
+          'Title' => 'WPScan Scan Information',
+          'TargetURL' => '{{ wpscan[scan_info.target_url] }}',
+          'WordpressVersion' => '{{ wpscan[scan_info.wordpress_version] }}',
+          'Plugins' => '{{ wpscan[scan_info.plugins_string] }}',
+          'Themes' => '{{ wpscan[scan_info.themes_string] }}',
+          'Users' => '{{ wpscan[scan_info.users] }}',
+          'WPScanVersion' => '{{ wpscan[scan_info.wpscan_version] }}',
+          'StartTime' => '{{ wpscan[scan_info.start_time] }}',
+          'TotalScanTime' => '{{ wpscan[scan_info.elapsed] }}'
+        },
+        'vulnerability' => {
+          'Title' => '{{ wpscan[vulnerability.title] }}',
+          'FixedIn' => '{{ wpscan[vulnerability.fixed_in] }}',
+          'CVE' => '{{ wpscan[vulnerability.cve] }}',
+          'References' => '{{ wpscan[vulnerability.url] }}',
+          'WPVulnDB' => '{{ wpscan[vulnerability.wpvulndb_url] }}'
+        }
+      }
     end
   end
 end
